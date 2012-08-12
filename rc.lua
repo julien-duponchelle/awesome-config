3061cc5c9ef59c48414fd21e95014d2dd42d13eb
Julien Duponchelle <julien@duponchelle.info>
2012-08-12 23:33:20 +0200

    Start chrome on screen two

--- rc.lua
+++ rc.lua
@@ -351,9 +351,9 @@ awful.rules.rules = {
       properties = { floating = true } },
     { rule = { class = "gimp" },
       properties = { floating = true } },
-    -- Set Firefox to always map on tags number 2 of screen 1.
-    -- { rule = { class = "Firefox" },
-    --   properties = { tag = tags[1][2] } },
+    -- Set Chromium to always map on tags number 1 of screen 2.
+     { rule = { class = "Chromium" },
+       properties = { tag = tags[2][1] } },
 }
 -- }}}
 
